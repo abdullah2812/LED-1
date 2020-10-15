ffbb772b76d7079c8a0cb74c8a0d1abeaa88c899
Admin-is-trator <72311114+Admin-is-trator@users.noreply.github.com>
2020-10-15 22:31:59 +0700

    Add files via upload

---
+++ Lamp.ino
@@ -0,0 +1,22 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "5JxE53KnwNincKaEqQwmHGSR8Hpd1M9f";
+char ssid[] = "request timed out";
+char pass[] = "ahyasatari21";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
